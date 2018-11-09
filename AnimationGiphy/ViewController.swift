5ab4cdc88b7e108b92e485e5a206225cc4d013aa
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-09 14:17:13 +0530

    Animation using Timer

--- AnimationGiphy/ViewController.swift
+++ AnimationGiphy/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var count = 1
+    func startAnimation()
+    {
+        
+        if timer.isValid {
+            timer.invalidate()
+            //self.count = 1
+            return
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (timer) in
+            let imageName : String = "File" + String(self.count) + ".gif"
+            self.count = self.count + 1
+            self.imageview.image = UIImage(named: imageName)
+            if self.count >= 71 {
+                timer.invalidate()
+                self.count = 1
+            }
+        })
+        
+    }
+    @IBAction func next(_ sender: Any) {
+        startAnimation()
+    }
+    @IBOutlet weak var imageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
