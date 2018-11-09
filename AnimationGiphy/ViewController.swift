30a49d6980311be0eee34f67a8832f852c595970
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-09 10:00:39 +0530

    Initial Commit

---
+++ AnimationGiphy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationGiphy
+//
+//  Created by Ravi Inder Manshahia on 09/11/18.
+//  Copyright © 2018 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
